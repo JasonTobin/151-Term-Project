db8a1e0e315ef3c8d8837aeabb5d02f6e604a0c3
Jason Tobin <94418504+JasonTobin@users.noreply.github.com>
2023-10-28 20:37:18 -0700

    Add files via upload

---
+++ src/application/Project.java
@@ -0,0 +1,86 @@
+package application;
+
+import java.time.LocalDate;
+import java.util.TreeMap;
+
+public class Project implements Comparable<Project> {
+    private String projName;
+    private LocalDate projDate; // Local Date to store date user chose
+    private String projDescription;
+    private TreeMap<String, Ticket> TicketCollector;
+
+    public boolean AddTicket(String n) { // Add a ticket to the project
+        TicketCollector.put(n, new Ticket(n));
+        return false;
+    }
+
+    public boolean AddTicket(String n, String d) {
+        TicketCollector.put(n, new Ticket(n));
+        return false;
+    }
+
+    // TODO: implement tickets and their insertion / deletion
+    public boolean DeleteTicket() {
+        // Figure out how tickets are stored
+        return false;
+    }
+
+    // Use this to store in some database in alphabetical order (learn SQL)
+    @Override
+    public int compareTo(Project e2) {
+        return this.projName.compareTo(e2.projName);
+    }
+
+    // Simple getters and setters and constructors
+
+    public void setProjName(String n) {
+        this.projName = n;
+        return;
+    }
+
+    public void setProjDate(LocalDate d) {
+        this.projDate = d;
+        return;
+    }
+
+    public void setProjDesc(String n) {
+        this.projDescription = n;
+        return;
+    }
+
+    public String getProjName() {
+        return this.projName;
+    }
+
+    public LocalDate getProjDate() {
+        return this.projDate;
+    }
+
+    public String getProjDesc() {
+        return this.projDescription;
+    }
+
+    public Project(String n, LocalDate p, String d) {
+        this.projDate = p;
+        this.projName = n;
+        this.projDescription = d;
+        this.TicketCollector = new TreeMap<String, Ticket>();
+    }
+
+    public Project(String n) { // Constructors for Project, must be a name for project
+        this.projName = n;
+        this.TicketCollector = new TreeMap<String, Ticket>();
+    }
+
+    public Project(String n, LocalDate p) {
+        this.projName = n;
+        this.projDate = p;
+        this.TicketCollector = new TreeMap<String, Ticket>();
+    }
+
+    public Project(String n, String d) {
+        this.projName = n;
+        this.projDescription = d;
+        this.TicketCollector = new TreeMap<String, Ticket>();
+    }
+}
